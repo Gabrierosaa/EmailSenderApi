d3e6c0397c3d2facdf5aab994304236742e6fd36
gabriel <gabrielflororosa2007@gmail.com>
2026-09-20 22:11:14 -0300

    Handlers

---
+++ EmailSenderApi.Application/Emails/Commands/SendEmail/SendEmailCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailSenderApi.Application.Emails.Commands.SendEmail
+{
+    internal class SendEmailCommandHandler
+    {
+    }
+}
